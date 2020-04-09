12c48efc328809c3cc6f855301ad08f2157b8dfc
toritheterrible <torimpham@gmail.com>
2020-04-08 20:11:36 -0700

    add to github

---
+++ client/build/precache-manifest.c80720f81d2a8979f44eb99233a12921.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "ce7b2e627414a33061e7",
+    "url": "/static/css/main.11620f9c.chunk.css"
+  },
+  {
+    "revision": "ce7b2e627414a33061e7",
+    "url": "/static/js/main.29aacc93.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "cb17bcf018bf7c55997e",
+    "url": "/static/js/2.09d13a7d.chunk.js"
+  },
+  {
+    "revision": "c6cc86e38e856402cfcd80d37758a9f8",
+    "url": "/index.html"
+  }
+];
